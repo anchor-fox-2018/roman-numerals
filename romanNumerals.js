f45cf39c014ec7353258fcbe2a5a16326751932d
anastasia <anastasia.santoso.id@gmail.com>
2018-09-03 15:47:36 +0700

    Updated testcase

--- romanNumerals.js
+++ romanNumerals.js
@@ -23,8 +23,9 @@ function toRoman(num) {
 
 console.log('My totally sweet testing script\n');
 console.log('input | expected | actual');
+console.log('______|__________|_______');
 console.log('4     | IV       | ', toRoman(4));
 console.log('9     | IX       | ', toRoman(9));
 console.log('23    | XXIII    | ', toRoman(23));
 console.log('1453  | MCDLIII  | ', toRoman(1453));
-console.log('1646  | MCDXLVI  | ', toRoman(1646));
+console.log('1646  | MDCXLVI  | ', toRoman(1646));
